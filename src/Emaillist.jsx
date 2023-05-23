ea6ad0829ae12b65fe3974c52df01bf94353d93e
Abhishek Panwar <imabhishek612@gmail.com>
2023-05-23 16:09:41 +0530

    emaillist done

--- src/Emaillist.jsx
+++ src/Emaillist.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./css/emaillist.css";
 import EmailListSetting from "./EmailListSetting";
+import Emailtype from "./Emailtype";
 const Emaillist = () => {
   return (
     <div className="emaillist">
       <EmailListSetting />
+      <Emailtype />
     </div>
   );
 };
